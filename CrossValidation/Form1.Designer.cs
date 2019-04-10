29d86c5ad0ee9714250af0462e93387d51dc5ca9
g3r4xd3on <38435412+g3r4xd3on@users.noreply.github.com>
2019-04-10 15:36:55 +0300

    Source Code
    Writed On Visual Studio 2017 using C# language.

---
+++ CrossValidation/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace CrossValidation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.automaticToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.dataGridTest = new System.Windows.Forms.DataGridView();
+            this.dataGridEducation = new System.Windows.Forms.DataGridView();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelData = new System.Windows.Forms.Label();
+            this.labelTest = new System.Windows.Forms.Label();
+            this.labelEducation = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDownK = new System.Windows.Forms.NumericUpDown();
+            this.manuelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.testDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.educationDataToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridTest)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridEducation)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownK)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.manuelToolStripMenuItem,
+            this.automaticToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1531, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exportToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.openToolStripMenuItem.Text = "Import";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.exportToolStripMenuItem.Text = "Export";
+            this.exportToolStripMenuItem.Click += new System.EventHandler(this.exportToolStripMenuItem_Click);
+            // 
+            // automaticToolStripMenuItem
+            // 
+            this.automaticToolStripMenuItem.Name = "automaticToolStripMenuItem";
+            this.automaticToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.automaticToolStripMenuItem.Text = "Automatic";
+            this.automaticToolStripMenuItem.Click += new System.EventHandler(this.automaticToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(500, 653);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 703);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1531, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(350, 17);
+            this.toolStripStatusLabel1.Text = "This program supports .xls and .xlsx file extension. Ready to open.";
+            // 
+            // dataGridTest
+            // 
+            this.dataGridTest.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridTest.Location = new System.Drawing.Point(518, 27);
+            this.dataGridTest.Name = "dataGridTest";
+            this.dataGridTest.Size = new System.Drawing.Size(500, 653);
+            this.dataGridTest.TabIndex = 10;
+            // 
+            // dataGridEducation
+            // 
+            this.dataGridEducation.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridEducation.Location = new System.Drawing.Point(1024, 27);
+            this.dataGridEducation.Name = "dataGridEducation";
+            this.dataGridEducation.Size = new System.Drawing.Size(500, 653);
+            this.dataGridEducation.TabIndex = 10;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(1483, 3);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown1.TabIndex = 13;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1432, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Test (%):";
+            // 
+            // labelData
+            // 
+            this.labelData.AutoSize = true;
+            this.labelData.Location = new System.Drawing.Point(12, 683);
+            this.labelData.Name = "labelData";
+            this.labelData.Size = new System.Drawing.Size(46, 13);
+            this.labelData.TabIndex = 18;
+            this.labelData.Text = "Rows: 0";
+            // 
+            // labelTest
+            // 
+            this.labelTest.AutoSize = true;
+            this.labelTest.Location = new System.Drawing.Point(515, 683);
+            this.labelTest.Name = "labelTest";
+            this.labelTest.Size = new System.Drawing.Size(46, 13);
+            this.labelTest.TabIndex = 19;
+            this.labelTest.Text = "Rows: 0";
+            // 
+            // labelEducation
+            // 
+            this.labelEducation.AutoSize = true;
+            this.labelEducation.Location = new System.Drawing.Point(1021, 683);
+            this.labelEducation.Name = "labelEducation";
+            this.labelEducation.Size = new System.Drawing.Size(46, 13);
+            this.labelEducation.TabIndex = 20;
+            this.labelEducation.Text = "Rows: 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1256, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Repeat Number (k):";
+            // 
+            // numericUpDownK
+            // 
+            this.numericUpDownK.Location = new System.Drawing.Point(1362, 3);
+            this.numericUpDownK.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownK.Name = "numericUpDownK";
+            this.numericUpDownK.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDownK.TabIndex = 22;
+            this.numericUpDownK.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // manuelToolStripMenuItem
+            // 
+            this.manuelToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.testDataToolStripMenuItem,
+            this.educationDataToolStripMenuItem1});
+            this.manuelToolStripMenuItem.Name = "manuelToolStripMenuItem";
+            this.manuelToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.manuelToolStripMenuItem.Text = "Manuel";
+            // 
+            // testDataToolStripMenuItem
+            // 
+            this.testDataToolStripMenuItem.Name = "testDataToolStripMenuItem";
+            this.testDataToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.testDataToolStripMenuItem.Text = "Test Data";
+            this.testDataToolStripMenuItem.Click += new System.EventHandler(this.testDataToolStripMenuItem_Click);
+            // 
+            // educationDataToolStripMenuItem1
+            // 
+            this.educationDataToolStripMenuItem1.Name = "educationDataToolStripMenuItem1";
+            this.educationDataToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.educationDataToolStripMenuItem1.Text = "Education Data";
+            this.educationDataToolStripMenuItem1.Click += new System.EventHandler(this.educationDataToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1531, 725);
+            this.Controls.Add(this.numericUpDownK);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridEducation);
+            this.Controls.Add(this.labelEducation);
+            this.Controls.Add(this.labelTest);
+            this.Controls.Add(this.labelData);
+            this.Controls.Add(this.dataGridTest);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cross Validation";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridTest)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridEducation)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownK)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.DataGridView dataGridTest;
+        private System.Windows.Forms.DataGridView dataGridEducation;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.Label labelData;
+        private System.Windows.Forms.Label labelTest;
+        private System.Windows.Forms.Label labelEducation;
+        private System.Windows.Forms.ToolStripMenuItem automaticToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDownK;
+        private System.Windows.Forms.ToolStripMenuItem manuelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem testDataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem educationDataToolStripMenuItem1;
+    }
+}
+
---
+++ CrossValidation/Form1.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using Microsoft.Office.Interop.Excel;
+
+namespace CrossValidation
+{
+    public partial class Form1 : Form
+    {
+        //Public Variables
+        string fileName;
+        int columnLenght = 0;
+        int rowLenght = 0;
+        int counterTest = 0;
+        int counterEducation = -1;
+        int rNumber;
+        List<int> usedNumbers = new List<int>();
+        HashSet<int> exclude = new HashSet<int>() { };
+        bool runOnceTest = false;
+        bool runOnceEducation = false;
+        List<int> numberPool = new List<int>();
+        int myNumber;
+        int missed = 0;
+        bool testDataClicked = false;
+        int testPercent;
+        bool error = true;
+        int k;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int GiveMeANumber()
+        {
+            var range = Enumerable.Range(0, 100).Where(i => !exclude.Contains(i));
+            var rand = new System.Random();
+            int index = rand.Next(0, rowLenght - exclude.Count);
+            return range.ElementAt(index);
+        }
+
+        private void open()
+        {
+            using (OpenFileDialog openFileDialog1 = new OpenFileDialog() { Filter = "Excel Workbook|*.xlsx|Excel WorkBook 97-2003|*.xls" })
+            {
+                if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    fileName = openFileDialog1.FileName;
+                    this.toolStripStatusLabel1.Text = fileName;
+                    toolStripStatusLabel1.Text = "Excel file loaded.";
+                }
+            }
+
+            //string PathConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileName + ";Extended Properties=\"Excel 8.0;HDR=Yes;\";"; //Only Support 97-2003 Excel File
+            string PathConn = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties = \"Excel 12.0 Xml;HDR=YES\"; ";
+            OleDbConnection conn = new OleDbConnection(PathConn);
+            try
+            {
+                OleDbDataAdapter myDataAdapter = new OleDbDataAdapter("Select * from [" + "Sayfa1" + "$]", conn);
+                System.Data.DataTable dt = new System.Data.DataTable();
+                myDataAdapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+
+                columnLenght = dataGridView1.ColumnCount;
+                rowLenght = dataGridView1.RowCount - 1;
+
+                toolStripStatusLabel1.Text = "[TR] File is on view. Ready to proceed.";
+                error = false;
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    OleDbDataAdapter myDataAdapter = new OleDbDataAdapter("Select * from [" + "Sheet1" + "$]", conn);
+                    System.Data.DataTable dt = new System.Data.DataTable();
+                    myDataAdapter.Fill(dt);
+                    dataGridView1.DataSource = dt;
+
+                    columnLenght = dataGridView1.ColumnCount;
+                    rowLenght = dataGridView1.RowCount - 1;
+
+                    toolStripStatusLabel1.Text = "[EN] File is on view. Ready to proceed.";
+                    error = false;
+                }
+                catch (Exception)
+                {
+                    toolStripStatusLabel1.Text = "Wrong language or file format!";
+                    error = true;
+                }
+            }
+            if (error == false)
+            {
+                int labelDataNumber = dataGridView1.RowCount - 1;
+                labelData.Text = "Rows:" + " " + labelDataNumber;
+                testPercent = Convert.ToInt32(Math.Floor(numericUpDown1.Value * Convert.ToDecimal(dataGridView1.RowCount - 1) / 100));
+                if (testPercent < 1)
+                {
+                    testPercent = 1;
+                }
+            }
+        }
+
+        private void testData()
+        {
+            if (error == false)
+            {
+                int labelDataNumber = dataGridView1.RowCount - 1;
+                labelData.Text = "Rows:" + " " + labelDataNumber;
+                testPercent = Convert.ToInt32(Math.Floor(numericUpDown1.Value * Convert.ToDecimal(dataGridView1.RowCount - 1) / 100));
+                if (testPercent < 1)
+                {
+                    testPercent = 1;
+                }
+            }
+
+            testDataClicked = true;
+            for (int o = 0; o < testPercent; o++)
+            {
+                counterTest++;
+                rNumber = GiveMeANumber();
+                exclude.Add(rNumber);
+                usedNumbers.Add(rNumber);
+
+                if (runOnceTest == false)
+                {
+                    int columnCounter = 1;
+                    for (int cT = 0; cT < columnLenght; cT++)
+                    {
+                        dataGridTest.Columns.Add("ColunmName", "Column" + columnCounter);
+                        columnCounter++;
+                    }
+                    runOnceTest = true;
+                }
+                DataGridViewRow row = (DataGridViewRow)dataGridTest.Rows[counterTest - 1].Clone();
+                for (int i = 0; i < columnLenght; i++)
+                {
+                    var item = dataGridView1.Rows[rNumber].Cells[i].Value;
+                    row.Cells[i].Value = item;
+                }
+                dataGridTest.Rows.Add(row);
+                toolStripStatusLabel1.Text = "Test data selection process is done.";
+            }
+
+            //This part gets rid of used lines in gridViewTest and gives revised numberPool list.
+
+            for (int i = 0; i <= rowLenght; i++)
+            {
+                numberPool.Add(i);
+            }
+            for (int a = 0; a < usedNumbers.Count(); a++)
+            {
+                for (int b = 0; b < numberPool.Count(); b++)
+                {
+                    if (numberPool[b] == usedNumbers[a])
+                    {
+                        numberPool[b] = -1;
+
+                    }
+                }
+            }
+            int labelTestNumber = dataGridTest.RowCount - 1;
+            labelTest.Text = "Rows:" + " " + labelTestNumber;
+        }
+
+        private void educationData()
+        {
+            if (testDataClicked == true)
+            {
+                for (int p = 0; p < rowLenght; p++)
+                {
+                    counterEducation++;
+                    if (runOnceEducation == false)
+                    {
+                        int columnCounter = 1;
+                        for (int cT = 0; cT < columnLenght; cT++)
+                        {
+                            dataGridEducation.Columns.Add("ColunmName", "Column" + columnCounter);
+                            columnCounter++;
+                        }
+                        runOnceEducation = true;
+                    }
+
+                    myNumber = numberPool[counterEducation];
+                    if (myNumber == -1)
+                    {
+                        missed = missed + 1;
+                    }
+                    else
+                    {
+                        DataGridViewRow row = (DataGridViewRow)dataGridEducation.Rows[counterEducation - missed].Clone();
+                        for (int i = 0; i < columnLenght; i++)
+                        {
+                            var item = dataGridView1.Rows[myNumber].Cells[i].Value;
+                            row.Cells[i].Value = item;
+                        }
+                        dataGridEducation.Rows.Add(row);
+                    }
+                }
+                toolStripStatusLabel1.Text = "Education data selection process is done.";
+            }
+            else
+            {
+                toolStripStatusLabel1.Text = "Please click \"Test Data\" first!";
+            }
+            int labelEducationNumber = dataGridEducation.RowCount - 1;
+            labelEducation.Text = "Rows:" + " " + labelEducationNumber;
+        }
+
+        private void export()
+        {
+            Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+            Workbook 
+                
+                wb1 = excelApp.Workbooks.Add(XlSheetType.xlWorksheet);
+            Worksheet ws1 = (Worksheet)excelApp.ActiveSheet;
+            excelApp.Visible = true;
+
+            //Test Data
+            for (int i = 1; i < dataGridTest.Columns.Count + 1; i++)
+            {
+                ws1.Cells[1, i] = dataGridTest.Columns[i - 1].HeaderText;
+            }
+            for (int i = 0; i < dataGridTest.Rows.Count - 1; i++)
+            {
+                for (int j = 0; j < dataGridTest.Columns.Count; j++)
+                {
+                    if (dataGridTest.Rows[i].Cells[j].Value == null || dataGridTest.Rows[i].Cells[j].Value == DBNull.Value || String.IsNullOrWhiteSpace(dataGridTest.Rows[i].Cells[j].Value.ToString()))
+                    {
+                        toolStripStatusLabel1.Text = "There are empty cells in table.";
+                    }
+                    else
+                    {
+                        ws1.Cells[i + 2, j + 1] = dataGridTest.Rows[i].Cells[j].Value.ToString();
+                    }
+                }
+            }
+
+            //Education Data
+            Workbook wb2 = excelApp.Workbooks.Add(XlSheetType.xlWorksheet);
+            Worksheet ws2 = (Worksheet)excelApp.ActiveSheet;
+            for (int i = 1; i < dataGridEducation.Columns.Count + 1; i++)
+            {
+                ws2.Cells[1, i] = dataGridEducation.Columns[i - 1].HeaderText;
+            }
+            for (int i = 0; i < dataGridEducation.Rows.Count - 1; i++)
+            {
+                for (int j = 0; j < dataGridEducation.Columns.Count; j++)
+                {
+                    if (dataGridEducation.Rows[i].Cells[j].Value == null || dataGridEducation.Rows[i].Cells[j].Value == DBNull.Value || String.IsNullOrWhiteSpace(dataGridEducation.Rows[i].Cells[j].Value.ToString()))
+                    {
+                        toolStripStatusLabel1.Text = "There are empty cells in table.";
+                    }
+                    else
+                    {
+                        ws2.Cells[i + 2, j + 1] = dataGridEducation.Rows[i].Cells[j].Value.ToString();
+                    }
+                }
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            open();
+        }
+
+        private void exportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            export();
+        }
+
+        private void automaticToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            k = Convert.ToInt32(numericUpDownK.Value);
+            if (error == false)
+            {
+                for (int i = 0; i < k; i++)
+                {
+                    dataGridTest.Rows.Clear();
+                    dataGridTest.Refresh();
+                    dataGridEducation.Rows.Clear();
+                    dataGridEducation.Refresh();
+                    counterTest = 0;
+                    counterEducation = -1;
+                    missed = 0;
+
+                    exclude.Clear();
+                    usedNumbers.Clear();
+                    numberPool.Clear();
+
+                    testData();
+                    educationData();
+                    export();
+                    
+                }
+            }
+        }
+
+        private void testDataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dataGridTest.Rows.Clear();
+            dataGridTest.Refresh();
+            dataGridEducation.Rows.Clear();
+            dataGridEducation.Refresh();
+            counterTest = 0;
+            counterEducation = -1;
+            missed = 0;
+
+            exclude.Clear();
+            usedNumbers.Clear();
+            numberPool.Clear();
+
+            testData();
+        }
+
+        private void educationDataToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            educationData();
+        }
+    }
+}
+
